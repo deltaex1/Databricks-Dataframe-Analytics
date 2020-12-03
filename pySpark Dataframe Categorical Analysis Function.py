d17e669b1c5470928cdfaf628439801a57a54dc8
Wil <35899839+deltaex1@users.noreply.github.com>
2020-12-03 17:07:00 -0500

    Update pySpark Dataframe Categorical Analysis Function.py

--- pySpark Dataframe Categorical Analysis Function.py
+++ pySpark Dataframe Categorical Analysis Function.py
@@ -12,7 +12,7 @@ def categorical_summarized(df, dfName, limit):
   rowCount = df.count()
   colCount = 1
 
-  print('***** Analysis for Report {}***** '.format(dfName))
+  print('***** Analysis for Report {} ***** '.format(dfName))
   print('Row Count: {}'.format(rowCount))
   print('Column Count: {}'.format(len(dfCol)))
   print(df.printSchema())
@@ -27,8 +27,8 @@ def categorical_summarized(df, dfName, limit):
     #get data type (may need new function to predict type)
     datatype = dict(df.dtypes)[col]
     print('Data Type: {}'.format(datatype))
-    if datatype != 'timestamp':
-      # count and % of non-nulls (isnan does not work with TimeStamps)
+    if datatype != 'timestamp' and datatype != 'boolean':
+      # count and % of non-nulls (isnan does not work with TimeStamps or Boolean)
       nullCount = df.filter((df[col] == "") | df[col].isNull() | isnan(df[col])).count()
     else:
       nullCount = df.filter((df[col] == "") | df[col].isNull()).count()
@@ -37,19 +37,22 @@ def categorical_summarized(df, dfName, limit):
     if nonNullCount != 0: #runs only if data rows are not null, nan, or blank
       nonNullpercent = round((rowCount - nullCount)/rowCount,2)
       print('Column Non-Null/NaN/Blank Count & %: {}, {}%'.format(nonNullCount,nonNullpercent*100))
-      # unique value count and %
-      # df.select(countDistinct(col)).show()
-      uniqueCount = df.select(countDistinct(col)).collect()[0][0]
-      print('Column Unique Count & %: {}, {}%'.format(uniqueCount, round(uniqueCount/nonNullCount*100,2)))
-          # value string length statistics
-      mincolLen = df.select(min(length(col))).collect()[0][0]
-      maxcolLen = df.select(max(length(col))).collect()[0][0]
-      avgcolLen = round(df.select(avg(length(col))).collect()[0][0],2)
-      if mincolLen == maxcolLen == avgcolLen: #prints UNIFORM if all 3 fields are equal
-        lenuniform = '- UNIFORM'
-      else: 
-        lenuniform = ''
-      print('String Length Avg/Min/Max: {}, {}, {} {}'.format(avgcolLen, mincolLen, maxcolLen, lenuniform))
+      if datatype != 'boolean': #skips length analysis if data type is Boolean
+        # unique value count and %
+        # df.select(countDistinct(col)).show()
+        uniqueCount = df.select(countDistinct(col)).collect()[0][0]
+        print('Column Unique Count & %: {}, {}%'.format(uniqueCount, round(uniqueCount/nonNullCount*100,2)))
+            # value string length statistics
+        mincolLen = df.select(min(length(col))).collect()[0][0]
+        maxcolLen = df.select(max(length(col))).collect()[0][0]
+        avgcolLen = round(df.select(avg(length(col))).collect()[0][0],2)
+        if mincolLen == maxcolLen == avgcolLen: #prints UNIFORM if all 3 fields are equal
+          lenuniform = '- UNIFORM'
+        else: 
+          lenuniform = ''
+        print('String Length Avg/Min/Max: {}, {}, {} {}'.format(avgcolLen, mincolLen, maxcolLen, lenuniform))
+      else:
+        uniqueCount = 2 #sets fixed value for Boolean data type
       # count of values (limits to top 10 or by limit)
       print('\nTop {} Distinct Values'.format(limit))
       df.groupBy(col).count().orderBy("count",ascending=False).limit(limit).show()
